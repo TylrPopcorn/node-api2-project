a76b1b25d2c8e1bdf0431ae4328c28c0269e476a
Tyler <tyler_mac@Rainbow-Trains-MacBook-Pro.local>
2022-08-09 23:01:45 -0400

    Finiished:

--- api/server.js
+++ api/server.js
@@ -1,2 +1,18 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require('express');
+const postsRouter = require('./posts/posts-router')
+
+const server = express()
+server.use(express.json())
+
+server.use("/api/posts", postsRouter)
+
+server.use("*", (req, res) => { //request, response
+    res.status(200).json({
+        message: "Success"
+    })
+})
+
+
+module.exports = server;
--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // require your server and launch it here
+
+/* Dependencies
+
+    npm install
+    npm i express
+
+*/
+
+const server = require('./api/server')
+
+const PORT = 8080;
+
+server.listen(PORT, () => {
+    console.log("test")
+})
+
+
+
